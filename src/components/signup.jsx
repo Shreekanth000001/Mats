0f921f164670daad3c872ab1869e82be353ede32
Shreekanth.k <Shreekanth.k000001@gmail.com>
2025-02-03 09:22:45 +0530

    Updates to signup component

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -4,24 +4,49 @@ import Logom from '../assets/logom.webp';
 
 const Signup = () => {
     const [randomNo, setRandomNo] = useState(0);
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [classmod, setClassmod] = useState('');
-    const [password, setPassword] = useState('');
+    const [steps, setSteps] = useState(1);
     const [message, setMessage] = useState('');
 
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [className, setClassName] = useState('');
+    const [description, setDescription] = useState('');
+    const [course, setCourse] = useState('');
+    const [section, setSection] = useState('');
+    const [strength, setStrength] = useState('');
+    const [numSubjects, setNumSubjects] = useState('');
+    const [subjects, setSubjects] = useState([]);
+
+    const handleSteps = (n) => setSteps((prev) => prev + n);
+
+
+    const handleNumSubjectsChange = (e) => {
+        let num = parseInt(e.target.value, 10) || 0;
+        num = Math.min(num, 10);  // Limit to 10
+        setNumSubjects(num);
+        setSubjects(Array(num).fill(''));
+    };
+
+
+    const handleSubjectChange = (index, value) => {
+        const newSubjects = [...subjects];
+        newSubjects[index] = value;
+        setSubjects(newSubjects);
+    };
 
     const submission = async (e) => {
         e.preventDefault();
 
-        const response = await fetch('/signup', {
+        const response = await fetch('http://localhost:3000/signup', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 name,
                 email,
-                classmod,
-                password
+                classmod: 'moderator',
+                password,
+                subjects
             })
         });
         const data = await response.json();
@@ -37,7 +62,7 @@ const Signup = () => {
     }
 
     const fetchuser = async (authToken) => {
-        const response = await fetch('/getuser', {
+        const response = await fetch('http://localhost:3000/getuser', {
             method: 'POST',
             headers: {
                 'authToken': authToken
@@ -47,11 +72,11 @@ const Signup = () => {
         sessionStorage.setItem('authToken', authToken);
         sessionStorage.setItem('userid', user._id);
         sessionStorage.setItem('username', user.name);
-        sessionStorage.setItem('classmod', user.classmod);
+        sessionStorage.setItem('classmod', 'moderator');
         savepass(user);
     }
     const savepass = async (user) => {
-        const response = await fetch('/userpass', {
+        const response = await fetch('http://localhost:3000/userpass', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
@@ -61,32 +86,89 @@ const Signup = () => {
         })
         const userpass = await response.json();
         if (!userpass.errors) {
-            userInfoSubmission(user);
+            userClassSubmission(user);
         } else {
             console.log(userpass.errors);
         }
     }
+    const userClassSubmission = async (user) => {
+        const response = await fetch('http://localhost:3000/signup/class', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                userid: user._id,
+                name: name,
+                description: description,
+                course: course,
+                section: section,
+                subjects: subjects,
+                strength: strength
+            })
+        })
+        const data = await response.json()
+        if (!response.ok) {
+            console.error(data.message);
+        } else {
+            navigate('/');
+        }
+
+    }
 
     return (
-        <div className='md:h-screen'>
+        <div className='md:min-h-screen'>
             <div className='bg-white py-5 shadow-md'>
-
                 <div className='mx-auto max-w-[90%] flex items-center'>
                     <img src={Logom} className='w-10 h-10' alt='' />
                     <p className='ml-1 font-bold text-3xl'>MATS</p>
                 </div>
             </div>
 
-            <section>
+            {steps === 1 && <section className='flex flex-col items-center justify-center md:min-h-screen'>
                 <header className='flex flex-col items-center justify-center text-center pb-4 mt-6'>
                     {message && <Alert message={message} bgcolor={'white'} newRandomNo={randomNo} />}
-
-                    <p className='font-bold text-2xl'>Class moderator profile creation</p>
-                    <p className='text-base text-[#52556e]'>Just a few questions before we begin</p>
+                    <p className='font-bold text-2xl'>Class Moderator Profile Creation</p>
+                    <p className='text-base text-[#52556e]'>Enter class details</p>
                 </header>
-                <main className='flex justify-center m-3'>
-                    <div className='flex justify-center items-center bg-custom-gradient w-full md:w-[428px] md:h-[460px] rounded-[30px]'>
-                        <form className=' flex flex-col items-center justify-center w-full bg-[#F6F7F9] md:bg-white md:w-[416px] md:h-[448px] rounded-3xl md:p-5' onSubmit={submission}>
+
+                <main className='flex justify-center m-3 w-full'>
+                    <div className='flex justify-center items-center bg-custom-gradient w-full md:w-[428px] md:max-h-[80vh] rounded-[30px] overflow-y-auto'>
+                        <form className='flex flex-col items-center justify-center w-full bg-[#F6F7F9] md:bg-white md:w-[416px] md:min-h-[448px] md:max-h-[75vh] rounded-3xl md:p-5 overflow-y-auto' onSubmit={submission}>
+                            <div className='bg-[#F6F7F9] w-full p-4 rounded-xl'>
+                                <p>Creating your moderator ID 🎉</p>
+                                <hr />
+                                <p className='font-bold p-2'></p>
+                                <div className='flex flex-col justify-center items-center space-y-2'>
+                                    <input className='bg-white rounded-3xl w-full h-[50px] px-4 ' type="name" name="className" placeholder='Class Name' required="" onChange={(e) => setClassName(e.target.value)} value={className} />
+                                    <input className='bg-white rounded-3xl w-full h-[50px] px-4 ' type="Description" name="description" placeholder='Description' required="" onChange={(e) => setDescription(e.target.value)} value={description} />
+                                    <input className='bg-white rounded-3xl w-full h-[50px] px-4 ' type="Course" name="course" placeholder='Course' required="" onChange={(e) => setCourse(e.target.value)} value={course} />
+                                    <input className='bg-white rounded-3xl w-full h-[50px] px-4 ' type="section" name="section" placeholder='Section' required="" onChange={(e) => setSection(e.target.value)} value={section} />
+                                    <input className='bg-white rounded-3xl w-full h-[50px] px-4 ' type="strength" name="strength" placeholder='strength' required="" onChange={(e) => setStrength(e.target.value)} value={strength} />
+                                    
+                                    <input className='bg-white rounded-3xl w-full h-[50px] px-4' type="number" placeholder='Number of Subjects' required onChange={handleNumSubjectsChange} value={numSubjects} />
+                                        <div className="overflow-y-auto max-h-[200px] w-full">
+                                            {subjects.map((subject, index) => (
+                                                <input key={index} className='bg-white rounded-3xl w-full h-[50px] px-4 my-1' type="text" placeholder={`Subject ${index + 1}`} required onChange={(e) => handleSubjectChange(index, e.target.value)} value={subject} />
+                                            ))}
+                                        </div>
+
+                                </div>
+                            </div>
+                            <button className='bg-[#020417] text-white mt-4 p-3 rounded-3xl w-fit' type="button" onClick={() => handleSteps(1)}>Next</button>
+                        </form>
+                    </div>
+                </main>
+            </section>}
+
+            {steps === 2 && <section>
+                <header className='flex flex-col items-center justify-center text-center pb-4 mt-6'>
+                        {message && <Alert message={message} bgcolor={'white'} newRandomNo={randomNo} />}
+
+                        <p className='font-bold text-2xl'>Create a learner profile</p>
+                        <p className='text-base text-[#52556e]'>Just a few questions before we begin</p>
+                    </header>
+                <main className='flex justify-center m-3 w-full'>
+                    <div className='flex justify-center items-center bg-custom-gradient w-full md:w-[428px] md:max-h-[80vh] rounded-[30px] overflow-y-auto'>
+                        <form className='flex flex-col items-center justify-center w-full bg-[#F6F7F9] md:bg-white md:w-[416px] md:min-h-[448px] md:max-h-[75vh] rounded-3xl md:p-5 overflow-y-auto' onSubmit={submission}>
                             <div className='bg-[#F6F7F9] w-full p-4 rounded-xl'>
                                 <p>Creating your moderator ID 🎉</p>
                                 <hr />
@@ -94,17 +176,17 @@ const Signup = () => {
                                 <div className='flex flex-col justify-center items-center space-y-2'>
                                     <input className='bg-white rounded-3xl w-full h-[50px] px-4 ' type="name" name="name" placeholder='Name' required="" onChange={(e) => setName(e.target.value)} value={name} />
                                     <input className='bg-white rounded-3xl w-full h-[50px] px-4 ' type="email" name="email" placeholder='Email' required="" onChange={(e) => setEmail(e.target.value)} value={email} />
-                                    <input className='bg-white rounded-3xl w-full h-[50px] px-4 ' type="email" name="email" placeholder='Class moderator name' required="" onChange={(e) => setClassmod(e.target.value)} value={classmod} />
-                                    <input className='bg-white rounded-3xl w-full h-[50px] px-4 ' type="password" name="password" placeholder='password' required="" onChange={(e) => setPassword(e.target.value)} value={password} />
+                                    <input className='bg-white rounded-3xl w-full h-[50px] px-4 ' type="password" name="password" placeholder='Password' required="" onChange={(e) => setPassword(e.target.value)} value={password} />
+
                                 </div>
                             </div>
-                            <button className='bg-[#020417] text-white mt-4 p-3 rounded-3xl w-fit' type="submit">I’m excited! Let’s do it</button>
+                            <button className='bg-[#020417] text-white p-3 rounded-3xl w-fit' type="submit">Submit</button>
                         </form>
                     </div>
-
                 </main>
-            </section>
+            </section>}
         </div>
+
     )
 }
 
