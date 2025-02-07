4cb3a1721583f943ede1c39b361794e2a3920f54
Shreekanth.k <Shreekanth.k000001@gmail.com>
2025-02-07 17:04:41 +0530

    Be the last update!

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import { BrowserRouter as Router, Routes, Route,useLocation  } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Home from './components/home'
 import Login from "./components/login";
 import Signup from "./components/signup";
 import Empty from "./components/empty";
 
-function App(){
+function App() {
   return (
-    <Router>
-      <MainLayout/>
+    <Router basename="/">
+      <MainLayout />
     </Router>
+
   );
 }
 function MainLayout() {
 
-  const location = useLocation ();
+  const location = useLocation();
   const isAuthloc = location.pathname === '/login' || location.pathname === '/signup';
 
   return (
-      <div className="App font-aileron md:bg-[#F6F7F9] tracking-wide">
-       {!isAuthloc && <Home />}
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="*" element={<Empty/>} />
-        </Routes>
-      </div>
+    <div className="App font-aileron md:bg-[#F6F7F9] tracking-wide">
+      {!isAuthloc && <Home />}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<Empty />} />
+      </Routes>
+    </div>
   );
 }
 
